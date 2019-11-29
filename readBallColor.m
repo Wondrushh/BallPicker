141ab7b4b86801a2248cdc8cc998b57923349bdc
Wondrushh <ukradeny.kocour@gmail.com>
2019-11-29 13:18:34 +0100

    Funkce konecne funguje, semgej dan do cyklu while

--- readBallColor.m
+++ readBallColor.m
@@ -1,34 +1,45 @@
-function colorVec = readBallColor(sensorPort,motorPort,colorVec)
+function colorVec = readBallColor(sensorPort,motorPort,colorVecInp)
 %% Funkce precte barvu kulicky, posune pasem a zapise kulicku do vektoru
 
       %sensorPort = port, do ktereho se pripoji RGB sensor
       %motorPort = port, do ktereho se pripoji motor, ktery hybe pasem
       %colorVec = vektor, do ktereho se uklada poradi kulicek
 %% telo funkce
+  colorVec = colorVecInp;
+  degreeTurn = 180;
+  defPower = -50;
   color = GetNXT2Color(sensorPort);
-  motA= NXTMotor(motorPort, 'Power',  0);
   switch color
     case 'RED'
-      motA.TachoLimit(90)
+      motA = NXTMotor(motorPort, 'Power', defPower, 'SpeedRegulation', false, 'TachoLimit', degreeTurn, 'ActionAtTachoLimit', 'HoldBrake');
       motA.SendToNXT()
       %prida se do vektoru 1
       colorVec(end+1) = 1;
+      disp('cervena')
+      pause(3);
     case 'GREEN'
-      motA.TachoLimit(90)
+      motA = NXTMotor(motorPort, 'Power', defPower, 'SpeedRegulation', false, 'TachoLimit', degreeTurn, 'ActionAtTachoLimit', 'HoldBrake');
       motA.SendToNXT()
       %prida se do vektoru 2
       colorVec(end+1) = 2;
+      disp('zelena')
+      pause(3);
     case 'BLUE'
-      motA.TachoLimit(90)
+      motA = NXTMotor(motorPort, 'Power', defPower, 'SpeedRegulation', false, 'TachoLimit', degreeTurn, 'ActionAtTachoLimit', 'HoldBrake');
       motA.SendToNXT()
       %prida se do vektoru 3
       colorVec(end+1) = 3;
+      disp('modra')
+      pause(3);
     case 'YELLOW'
-      motA.TachoLimit(90)
+      motA = NXTMotor(motorPort, 'Power', defPower, 'SpeedRegulation', false, 'TachoLimit', degreeTurn, 'ActionAtTachoLimit', 'HoldBrake');
       motA.SendToNXT()
       %prida se do vektoru 4
       colorVec(end+1) = 4;
+      disp('zluta')
+      pause(3);
+    otherwise
+      pause(3)
   end
-  
-  pause(1);
+
 end
--- semgej.m
+++ semgej.m
@@ -1,14 +1,16 @@
 %% musi tam byt
 COM_CloseNXT all
-clc, clear, close;
+clc, clear, close
 
 nxt = COM_OpenNXT();
 COM_SetDefaultNXT(nxt);
 %% nastaveni senzoru
 colors = [];
 OpenNXT2Color(SENSOR_4,'FULL');
-%precte barvu a otoci motorem kdyz je tam kulicka
-readBallColor(SENSOR_4,'A',colors);
+while 1
+  %precte barvu a otoci motorem kdyz je tam kulicka
+  colors = readBallColor(SENSOR_4,'A',colors);
+end
 
 
 %% musi tam byt
