94392aebee68fd6383d7509d25988a48f2b64625
Wondrushh <ukradeny.kocour@gmail.com>
2019-12-05 10:50:18 +0100

    Zmenen princip otaceni robota, predelan Tacho na power za cas

--- goTurn.m
+++ goTurn.m
@@ -1,26 +1,27 @@
 function [turnDirection] = goTurn(motorLeft, motorRight, speed, turnDirectionInp)
 %Tato funkce slouzi k tomu, aby robot otocil svuj smer pohybu.
 %Udela se to, ze se robot otoci na jednom kole
-  motLeft  = NXTMotor(motorLeft, 'Power',  10, 'TachoLimit', 3, 'ActionAtTachoLimit', 'Brake'); %zastavi motory , 'TachoLimit', 0, 'ActionAtTachoLimit', 'HoldBrake'
-  motRight = NXTMotor(motorRight, 'Power', 10, 'TachoLimit', 3, 'ActionAtTachoLimit', 'Brake'); %zastavi motory
+  motLeft  = NXTMotor(motorLeft, 'Power',  0); %'TachoLimit', 3, 'ActionAtTachoLimit', 'Brake'); %zastavi motory , 'TachoLimit', 0, 'ActionAtTachoLimit', 'HoldBrake'
+  motRight = NXTMotor(motorRight, 'Power', 0);% 'TachoLimit', 3, 'ActionAtTachoLimit', 'Brake'); %zastavi motory
 
   motLeft.SendToNXT();
   motRight.SendToNXT();
   pause(0.1)
-  degrees = 2160;
+%   degrees = 2160;
     switch turnDirectionInp
       case 0 %doleva
-        motLeft  = NXTMotor(motorLeft, 'Power',  -5, 'TachoLimit', degrees/speed, 'ActionAtTachoLimit', 'Brake');
-        motRight = NXTMotor(motorRight, 'Power', speed, 'TachoLimit', degrees, 'ActionAtTachoLimit', 'Brake');
+        motLeft  = NXTMotor(motorLeft, 'Power',  -5);
+        motRight = NXTMotor(motorRight, 'Power', speed);
         motLeft.SendToNXT();
         motRight.SendToNXT();
         pause(10)
         turnDirection = 1;
       case 1 %doprava
-        motLeft  = NXTMotor(motorLeft, 'Power',  speed, 'TachoLimit', degrees, 'ActionAtTachoLimit', 'Brake');
-        motRight = NXTMotor(motorRight, 'Power', -5, 'TachoLimit', degrees/speed, 'ActionAtTachoLimit', 'Brake');
+        motLeft  = NXTMotor(motorLeft, 'Power',  speed); %'TachoLimit', degrees, 'ActionAtTachoLimit', 'Brake');
+        motRight = NXTMotor(motorRight, 'Power', -5); %'TachoLimit', degrees/speed, 'ActionAtTachoLimit', 'Brake');
         motLeft.SendToNXT();
         motRight.SendToNXT();
+        pause(10)
         turnDirection = 0;
     end
 end
