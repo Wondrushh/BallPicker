e0bdb01f26c1afef68cf47d462b5ed186a2cd7f1
Wondrushh <ukradeny.kocour@gmail.com>
2019-12-07 14:48:12 +0100

    minor

--- goTurn.m
+++ goTurn.m
@@ -3,6 +3,8 @@ function [turnDirection] = goTurn(motorLeft, motorRight, speed, turnDirectionInp
 %Udela se to, ze se robot otoci na jednom kole
   motLeft  = NXTMotor(motorLeft, 'Power', 0); 
   motRight = NXTMotor(motorRight, 'Power', 0);
+  
+  secondWheelSpeed = -(speed/4);
 
   motLeft.SendToNXT();
   motRight.SendToNXT();
@@ -10,7 +12,7 @@ function [turnDirection] = goTurn(motorLeft, motorRight, speed, turnDirectionInp
 %   degrees = 2160;
     switch turnDirectionInp
       case 0 %doleva
-        motLeft  = NXTMotor(motorLeft, 'Power',  -speed/4);
+        motLeft  = NXTMotor(motorLeft, 'Power',  secondWheelSpeed);
         motRight = NXTMotor(motorRight, 'Power', speed);
         motLeft.SendToNXT();
         motRight.SendToNXT();
@@ -24,7 +26,7 @@ function [turnDirection] = goTurn(motorLeft, motorRight, speed, turnDirectionInp
         pause(0.5);
       case 1 %doprava
         motLeft  = NXTMotor(motorLeft, 'Power',  speed); 
-        motRight = NXTMotor(motorRight, 'Power', -speed/4); 
+        motRight = NXTMotor(motorRight, 'Power', secondWheelSpeed); 
         motLeft.SendToNXT();
         motRight.SendToNXT();
         pause(0.38*speed);
