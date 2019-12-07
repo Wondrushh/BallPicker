2b37976df13554cf3503ff5ed139df1b8200e1e9
Wondrushh <ukradeny.kocour@gmail.com>
2019-12-07 14:39:23 +0100

    minor

--- goTurn.m
+++ goTurn.m
@@ -1,8 +1,8 @@
 function [turnDirection] = goTurn(motorLeft, motorRight, speed, turnDirectionInp)
 %Tato funkce slouzi k tomu, aby robot otocil svuj smer pohybu.
 %Udela se to, ze se robot otoci na jednom kole
-  motLeft  = NXTMotor(motorLeft, 'Power',  0); %'TachoLimit', 3, 'ActionAtTachoLimit', 'Brake'); %zastavi motory , 'TachoLimit', 0, 'ActionAtTachoLimit', 'HoldBrake'
-  motRight = NXTMotor(motorRight, 'Power', 0);% 'TachoLimit', 3, 'ActionAtTachoLimit', 'Brake'); %zastavi motory
+  motLeft  = NXTMotor(motorLeft, 'Power', 0); 
+  motRight = NXTMotor(motorRight, 'Power', 0);
 
   motLeft.SendToNXT();
   motRight.SendToNXT();
@@ -17,14 +17,14 @@ function [turnDirection] = goTurn(motorLeft, motorRight, speed, turnDirectionInp
         pause(0.38*speed);
         turnDirection = 1;
         motLeft  = NXTMotor(motorLeft, 'Power',  0); %'TachoLimit', 3, 'ActionAtTachoLimit', 'Brake'); %zastavi motory , 'TachoLimit', 0, 'ActionAtTachoLimit', 'HoldBrake'
-        motRight = NXTMotor(motorRight, 'Power', 0);% 'TachoLimit', 3, 'ActionAtTachoLimit', 'Brake'); %zastavi motory
+        motRight = NXTMotor(motorRight, 'Power', 0);
         
         motLeft.SendToNXT();
         motRight.SendToNXT();
         pause(0.5);
       case 1 %doprava
-        motLeft  = NXTMotor(motorLeft, 'Power',  speed); %'TachoLimit', degrees, 'ActionAtTachoLimit', 'Brake');
-        motRight = NXTMotor(motorRight, 'Power', -speed/4); %'TachoLimit', degrees/speed, 'ActionAtTachoLimit', 'Brake');
+        motLeft  = NXTMotor(motorLeft, 'Power',  speed); 
+        motRight = NXTMotor(motorRight, 'Power', -speed/4); 
         motLeft.SendToNXT();
         motRight.SendToNXT();
         pause(0.38*speed);
