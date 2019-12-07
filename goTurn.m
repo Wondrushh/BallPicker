a214fdd38f6b156df6559b6d515e1cbae0ff773f
Wondrushh <ukradeny.kocour@gmail.com>
2019-12-07 14:56:20 +0100

    minor

--- goTurn.m
+++ goTurn.m
@@ -4,12 +4,11 @@ function [turnDirection] = goTurn(motorLeft, motorRight, speed, turnDirectionInp
   motLeft  = NXTMotor(motorLeft, 'Power', 0); 
   motRight = NXTMotor(motorRight, 'Power', 0);
   
-  secondWheelSpeed = -(speed/4);
+  secondWheelSpeed = ceil(-(speed/4));
 
   motLeft.SendToNXT();
   motRight.SendToNXT();
   pause(0.05)
-%   degrees = 2160;
     switch turnDirectionInp
       case 0 %doleva
         motLeft  = NXTMotor(motorLeft, 'Power',  secondWheelSpeed);
@@ -31,8 +30,8 @@ function [turnDirection] = goTurn(motorLeft, motorRight, speed, turnDirectionInp
         motRight.SendToNXT();
         pause(0.38*speed);
         turnDirection = 0;
-        motLeft  = NXTMotor(motorLeft, 'Power',  0); %'TachoLimit', 3, 'ActionAtTachoLimit', 'Brake'); %zastavi motory , 'TachoLimit', 0, 'ActionAtTachoLimit', 'HoldBrake'
-        motRight = NXTMotor(motorRight, 'Power', 0);% 'TachoLimit', 3, 'ActionAtTachoLimit', 'Brake'); %zastavi motory
+        motLeft  = NXTMotor(motorLeft, 'Power',  0); 
+        motRight = NXTMotor(motorRight, 'Power', 0);
         
         motLeft.SendToNXT();
         motRight.SendToNXT();
