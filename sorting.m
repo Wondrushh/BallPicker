786a95ba7df6e00a4a0360ad67379ea4bc71614a
Wondrushh <ukradeny.kocour@gmail.com>
2019-12-10 23:21:00 +0100

    Pridana kostra funkce ktera roztridi kulicky.

---
+++ sorting.m
@@ -0,0 +1,8 @@
+function sorting(conveyorMotor, colorVec, rearColorSensor)
+%Robot zacne vysypavat kulicky dle barvy na zemi
+%   Robot nejdriv zpozoruje cervenou, pote zpomali a ceka na to az najde
+%   pozadovanou barvu kterou ma v zasobniku
+  
+
+end
+
